1a6aaa213951932bc4f2362ad9affe2769cffeee
charlyecastro <castroc@uw.edu>
2019-04-15 21:27:31 -0700

    refactored widgets

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import './views/video_cell.dart';
 
 void main() => runApp(RealWorldApp());
 
@@ -22,7 +23,7 @@ class RealWordState extends State<RealWorldApp> {
     final response = await http.get(url);
     //print(response);
 
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       print(response.body);
       final map = json.decode(response.body);
       final videosJson = map["videos"];
@@ -38,51 +39,34 @@ class RealWordState extends State<RealWorldApp> {
 
   @override
   Widget build(BuildContext context) {
-    
     return new MaterialApp(
       home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("REAL WORLD APP BAR"),
-          actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.refresh),
-            onPressed: (){
-              print("reloading");
-              setState(() {
-                _isLoading = true;
-              });
-              _fetchData();
-            },)
-          ],
-        ),
-        body: new Center(
-          child: _isLoading ? new CircularProgressIndicator() : 
-          new ListView.builder(
-            itemCount: this.videos != null ? this.videos.length : 0,
-            itemBuilder: (context, i) {
-              final video = this.videos[i];
-              return new  Column(
-                children: <Widget>[
-                  new Container(
-                    padding: new EdgeInsets.all(16.0),
-                    child: new Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        new Image.network(video["imageUrl"]),
-                        new Container(height:8.0),
-                        new Text(video["name"],
-                          style: new TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold))
-                      ],
-                    ),
-                  ),
-                  new Divider()
-                ],
-              );
-              //return new Text("Row: $i");
-            },
+          appBar: new AppBar(
+            title: new Text("REAL WORLD APP BAR"),
+            actions: <Widget>[
+              new IconButton(
+                icon: new Icon(Icons.refresh),
+                onPressed: () {
+                  print("reloading");
+                  setState(() {
+                    _isLoading = true;
+                  });
+                  _fetchData();
+                },
+              )
+            ],
           ),
-        )
-      ),
+          body: new Center(
+            child: _isLoading
+                ? new CircularProgressIndicator()
+                : new ListView.builder(
+                    itemCount: this.videos != null ? this.videos.length : 0,
+                    itemBuilder: (context, i) {
+                      final video = this.videos[i];
+                      return new VideoCell(video);
+                    },
+                  ),
+          )),
     );
   }
 }
-
