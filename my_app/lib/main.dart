cb945165e6ff923ab58e33b71eed2c05d0d49a5b
charlyecastro <castroc@uw.edu>
2019-04-15 21:19:56 -0700

    rendering UI

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -13,6 +13,7 @@ class RealWorldApp extends StatefulWidget {
 
 class RealWordState extends State<RealWorldApp> {
   var _isLoading = false;
+  var videos;
 
   _fetchData() async {
     print("FETCHING DATA!");
@@ -25,9 +26,10 @@ class RealWordState extends State<RealWorldApp> {
       print(response.body);
       final map = json.decode(response.body);
       final videosJson = map["videos"];
-      videosJson.forEach((video){
-        print(video["name"]);
-      });
+      this.videos = videosJson;
+      // videosJson.forEach((video){
+      //   print(video["name"]);
+      // });
       setState(() {
         _isLoading = false;
       });
@@ -53,7 +55,31 @@ class RealWordState extends State<RealWorldApp> {
           ],
         ),
         body: new Center(
-          child: _isLoading ? new CircularProgressIndicator() : new Text("Finished Loading..."),
+          child: _isLoading ? new CircularProgressIndicator() : 
+          new ListView.builder(
+            itemCount: this.videos != null ? this.videos.length : 0,
+            itemBuilder: (context, i) {
+              final video = this.videos[i];
+              return new  Column(
+                children: <Widget>[
+                  new Container(
+                    padding: new EdgeInsets.all(16.0),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Image.network(video["imageUrl"]),
+                        new Container(height:8.0),
+                        new Text(video["name"],
+                          style: new TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                  ),
+                  new Divider()
+                ],
+              );
+              //return new Text("Row: $i");
+            },
+          ),
         )
       ),
     );
